f9ef37a7eac0915adf16846c04d4a7382d0f5f72
Lenovo <Lenovo@Lenovo-PC>
2019-04-13 16:11:07 +0530

    Framework code pushed

---
+++ com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/DemoForMaven.java
@@ -0,0 +1,12 @@
+package com.learnautomation.selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoForMaven {
+	
+	@Test
+	public void test1() {
+		System.out.println("Welcome to Maven World");
+	}
+
+}
